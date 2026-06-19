76e1db00a18438b63e5a8d968851f5432a8bb370
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-06-19 10:35:08 +0530

    implement email verification functionality, including OTP input handling and API integration, and update navbar for email verification

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,11 +2,42 @@ import { useNavigate } from 'react-router-dom';
 import { assets } from '../assets/assets';
 import { useContext } from 'react';
 import { AppContent } from '../context/appContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const { userData, backendUrl, setUserData, setIsLoggedin } =
     useContext(AppContent);
+
+  const sentVerificatioOtp = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(
+        backendUrl + '/api/auth/send-verify-otp',
+      );
+      if (data.success) {
+        navigate('/email-verify');
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(backendUrl + '/api/auth/logout');
+      data.success && setIsLoggedin(false);
+      data.success && setUserData(false);
+      navigate('/');
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-4 sm:px-24 absolute top-0">
@@ -16,10 +47,19 @@ const Navbar = () => {
           {userData.name[0].toUpperCase()}
           <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
             <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
-              <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">
-                Verify Email
-              </li>
-              <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10">
+              {!userData.isAccountVerified && (
+                <li
+                  onClick={sentVerificatioOtp}
+                  className="py-1 px-2 hover:bg-gray-200 cursor-pointer"
+                >
+                  Verify Email
+                </li>
+              )}
+
+              <li
+                onClick={logout}
+                className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10"
+              >
                 Logout
               </li>
             </ul>
